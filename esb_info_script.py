e781966dbbc5dcf665a7d578f0d8415381eb7f8f
Sergio Rodríguez Calvo <serrodcal@MacBook-Pro-de-Sergio.local>
2016-07-20 09:43:18 +0200

    Create main and extract xml files function

--- esb_info_script.py
+++ esb_info_script.py
@@ -1,1 +1,23 @@
-.
+import sys, os
+
+def get_xml_file(files):
+    res = ()
+    for file in files:
+        if file.endswith(".xml"):
+            res.append(file)
+    return res
+
+if __name__ == '__main__':
+    if len(sys.argv)>1:
+        directory_path = sys.argv[1]
+        if os.path.isdir(directory_path) and os.path.exists(directory_path):
+            files = os.listdir(directory_path)
+            if files and len(files)>0:
+                xml_files = get_xml_file(files)
+            else:
+                print("There are not any file in this directory")
+        else:
+            print("Argument is not a valid directory path")
+    else:
+        print("There are not arguments")
+
