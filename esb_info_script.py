41ee5f1f63a26567701261fddf795f159be303b7
Sergio Rodríguez Calvo <serrodcal@MacBook-Pro-de-Sergio.local>
2016-07-20 10:46:10 +0200

    Create strcture function of script

--- esb_info_script.py
+++ esb_info_script.py
@@ -1,11 +1,35 @@
 import sys, os
 
 def get_xml_file(files):
-    res = ()
+    res = list()
     for file in files:
         if file.endswith(".xml"):
             res.append(file)
     return res
+
+def get_file_content_into_string(directory, file):
+    data = ""
+    full_path = directory + "/" + file
+    with open(full_path, 'r') as myfile:
+        data=myfile.read().replace('\n', '')
+    return data
+
+def get_list_of_api_info(directory, xml_files):
+    apis_info = list()
+    for api_file in xml_files:
+        file_content = get_file_content_into_string(directory, api_file)
+        api_info = extract_info(file_content)
+        apis_info.append(api_info)
+    return apis_info
+
+def extract_info(api_content):
+    return None
+
+def parse_info_to_json(apis_info):
+    return None
+
+def put_json_in_this_directory(apis_json):
+    var = None
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
@@ -14,6 +38,9 @@ if __name__ == '__main__':
             files = os.listdir(directory_path)
             if files and len(files)>0:
                 xml_files = get_xml_file(files)
+                apis_info = get_list_of_api_info(directory_path, xml_files)
+                apis_json = parse_info_to_json(apis_info)
+                put_json_in_this_directory(apis_json)
             else:
                 print("There are not any file in this directory")
         else:
